21e988423b6387c2cad7ca55a0b31a332b77b701
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-07-08 08:35:30 -0300

    First commit

---
+++ src/test/java/com/rddev/springsecurityldap/SpringSecurityLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rddev.springsecurityldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
